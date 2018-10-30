47bed273a388400b3265e910406f4da8beddf28a
Gustavo <gustavofn@gmail.com>
2018-10-30 10:41:50 -0700

    added init_sample2.lua

---
+++ init_sample2.lua
@@ -0,0 +1,27 @@
+print("Booting up ESP8266...")
+-- Led settings
+local led=4
+gpio.mode(led,gpio.OUTPUT)
+
+-- Wifi Configuration
+wifi.setmode(wifi.STATIONAP)
+wifi.ap.config({ssid="ESP12-E123", auth=wifi.OPEN})
+enduser_setup.manual(true)
+enduser_setup.start(
+  function()
+    print("Connected to wifi as:" .. wifi.sta.getip())
+  end,
+  function(err, str)
+    print("enduser_setup: Err #" .. err .. ": " .. str)
+  end
+);
+
+-- Reads DHT11 data every 20 seconds and send to endpoint
+dofile("dht11.lua")
+tmr.alarm(1,20000,1,function()
+  if wifi.sta.getip() == nil then
+    print('No connection, waiting...')
+  else
+    getTemp()
+  end
+end)
